43a3e54f8351479b1dcb4caf58225c2a028ac708
a2023951504 <a2023951504@teiacoltec.org>
2024-05-15 14:21:52 -0300

    Criação da classe Biblioteca.

---
+++ src/Biblioteca.java
@@ -0,0 +1,22 @@
+public class Biblioteca {
+
+    //Atributos
+    private Livros[] livros;
+    private Usuario[] usuarios;
+    private String nome;
+
+    //Métodos
+    static void main(String[]): método por onde iniciará a execução do programa
+
+    void cadastrarLivro(): deverá cadastrar um novo livro na biblioteca
+
+    void cadastrarUsuario(): deverá cadastrar um novo usuário na biblioteca
+
+    void realizarEmprestimo(): deverá inicializar o processo de empréstimo
+
+    void realizarDevolucao(): deverá iniciar o processo de devolução
+
+    void imprimirLivros(): deverá imprimir todos os livros disponíveis na biblioteca
+
+
+}
